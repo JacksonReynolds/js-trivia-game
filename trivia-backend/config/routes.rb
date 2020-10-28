b7e9a7498d41fad558367f3036a465e9018d1d57
JacksonReynolds <jwreynod@gmail.com>
2020-10-28 01:15:44 -0400

    dumb route typo

--- trivia-backend/config/routes.rb
+++ trivia-backend/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :questions
   resources :users
 
-  get '/users/hiscores', to: 'users#hiscores' 
+  get '/hiscores', to: 'users#hiscores' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
